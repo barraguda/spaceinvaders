2ac19bac7446d81671ca787a53011fe462d09f0e
bitful-pannul <bitful-pancreas@protonmail.com>
2024-07-30 15:20:55 +0300

    ez game

---
+++ spaceinvaders/src/lib.rs
@@ -0,0 +1,20 @@
+use kinode_process_lib::{await_message, call_init, homepage, http, println, Address};
+static SPACE_SVG: &str = include_str!("../../pkg/ui/space.svg");
+
+wit_bindgen::generate!({
+    path: "target/wit",
+    world: "process-v0",
+});
+
+call_init!(init);
+fn init(our: Address) {
+    println!("hello ööö");
+
+    http::serve_index_html(&our, "ui", false, false, vec!["/"]).unwrap();
+    homepage::add_to_homepage("space invaders", Some(SPACE_SVG), Some("/"), None);
+    loop {
+        match await_message() {
+            _ => {}
+        };
+    }
+}
